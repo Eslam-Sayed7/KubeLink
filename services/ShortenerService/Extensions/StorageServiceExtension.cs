e7d557deed7adc70923c6efcd5ca81e0d0fea008
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2025-08-07 19:44:49 +0300

    Add:ApplyMigrationExtension for shortenService , update the deployment for auth

--- services/ShortenerService/Extensions/StorageServiceExtension.cs
+++ services/ShortenerService/Extensions/StorageServiceExtension.cs
@@ -7,7 +7,9 @@ public static class StorageServiceExtension
 {
     public static void RegisterStorageService(this WebApplicationBuilder builder)
     {
-        builder.Services.AddDbContext<ShortnerDbContext>(x => x.UseNpgsql(Environment.GetEnvironmentVariable("POSTGRES_CONNECTION")));
+        builder.Services.AddDbContext<ShortnerDbContext>(x => x.UseNpgsql( 
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        ));
     }
 
 }
