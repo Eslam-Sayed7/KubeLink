041326df304e6cebef521b4fd8ed803292cb1364
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2025-08-16 13:26:41 +0300

    Add resolve func

---
+++ services/ShortenerService-go/Api/database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import(
+	"context"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var Ctx = context.Background()
+
+func CreatClient() *redis.Client {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB: dbNo,
+	})
+
+	return rdb
+}
+
+
